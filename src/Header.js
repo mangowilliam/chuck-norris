4fd8b4fa5cd0cba2b23a36218ed25815b98e4522
mangowilliam <williammango2015@gmail.com>
2021-08-31 14:51:39 +0300

    add nav bar

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className="header">
-            <h1>Common sense media</h1>
+            <div className="header_icon">
+                <h1>Common sense media</h1>
+            </div>
+            <div className="header_icon">
+                <input type="" placeholder="find movies, 
+                books, and more ..."></input>
+                <SearchIcon/>
+            </div>
         </div>
     )
 }
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Nav.css";
+function Nav() {
+    return (
+        <div clasName="nav">  
+            <h2>Movies</h2>
+            <h2>Books</h2>
+            <h2>Apps</h2>
+            <h2>parents</h2>
+            <h2>Latino</h2>
+            <h2>Research</h2>
+            <h2>about Us</h2>
+            <h2>Distance Learning</h2>
+        </div>
+    )
+}
+
+export default Nav
