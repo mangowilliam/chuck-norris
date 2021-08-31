3edddde199b1318b695046298ff5e85809f663fc
mangowilliam <williammango2015@gmail.com>
2021-08-31 21:03:38 +0300

    consume api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Nav from './Nav';
+import axios from 'axios';
 
 function App() {
+  const [state, seState] = useState({
+    joke:""
+  })
+  useEffect(async() => {
+   fetchData();
+  }, []);
+  const fetchData= async() => {
+    const result = await axios.get('https://api.chucknorris.io/jokes/random');
+    console.log(result.data.value);
+    setState({ 
+      ...state,
+      joke: result.data.value
+    })
+   },
+
   return (
     <div className="App">
         <Header />
         <Nav />
+        <div className="jumbotron">
+          <h3>Random joke</h3>
+          <p>joke</p>
+          <h3>science joke</h3>
+          <p>joke</p>
+        </div>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -2,15 +2,15 @@ import React from 'react'
 import "./Nav.css";
 function Nav() {
     return (
-        <div clasName="nav">  
-            <h2>Movies</h2>
-            <h2>Books</h2>
-            <h2>Apps</h2>
-            <h2>parents</h2>
-            <h2>Latino</h2>
-            <h2>Research</h2>
-            <h2>about Us</h2>
-            <h2>Distance Learning</h2>
+        <div className="nav">  
+            <h2>Random</h2>
+            <h2>Science</h2>
+            <h2>Food</h2>
+            <h2>Fashion</h2>
+            <h2>Movie</h2>
+            <h2>Music</h2>
+            <h2>Money</h2>
+            <h2>Sport</h2>
         </div>
     )
 }
